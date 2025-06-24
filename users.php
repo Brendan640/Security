9016e1a8dcdbd2aa967b7fdc5a672c4af734b6c6
Brendan640 <B.kuiper07@student.alfa-college.nl>
2025-06-24 09:09:59 +0200

    Add files via upload

--- users.php
+++ users.php
@@ -2,7 +2,7 @@
 session_start();
 include 'includes/db.php';
 
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== false){
     header("location: index.php");
     exit;
 }
